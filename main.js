04087901c0b2fb695c3e496369deb834ac6312a7
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-01-29 19:19:35 -0500

    Initial test of hareactive

--- main.js
+++ main.js
@@ -1,5 +1,66 @@
-import sync from "/web_modules/framesync.js";
+// import sync from "/web_modules/framesync.js";
+import {
+  stepperFrom,
+  sample,
+  lift,
+  Behavior
+} from "/web_modules/@funkia/hareactive.js";
 
-console.log("Hello, world!");
+import {
+  streamFromEvent,
+  render
+} from "/web_modules/@funkia/hareactive/dom.js";
 
-sync.update(({ timestamp }) => void console.log(timestamp), true);
+class Image {
+  render(context) {
+    context.fillStyle = "rgb(200, 0, 0)";
+    context.fillRect(0, 0, 50, 50);
+  }
+}
+
+class MoveImage extends Image {
+  constructor(v, parent) {
+    super();
+    this.v = v;
+    this.parent = parent;
+  }
+
+  render(context) {
+    const [x, y] = this.v;
+    context.save();
+    context.translate(x, y);
+    this.parent.render(context);
+    context.restore();
+  }
+}
+
+const move = (vB, imageB) => {
+  return lift((v, image) => new MoveImage(v, image), vB, imageB);
+};
+
+const image = new Image();
+
+const rect = Behavior.of(image);
+
+const canvas = document.querySelector("canvas");
+canvas.width = 400;
+canvas.height = 400;
+const context = canvas.getContext("2d");
+
+const mouseBehavior = stepperFrom(
+  [0, 0],
+  streamFromEvent(window, "mousemove").map(({ clientX, clientY }) => [
+    clientX,
+    clientY
+  ])
+);
+
+const mouse = sample(mouseBehavior).run();
+
+const imageBehavior = move(mouse, rect);
+
+render(i => {
+  context.fillStyle = "rgba(255, 255, 255, 0.1)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  i.render(context);
+}, imageBehavior);
