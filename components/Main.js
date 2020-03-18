ce5ef13ed9e1188c4d922da65456ccfe92bd65c7
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-03-18 19:38:59 -0400

    Move app into Main component

---
+++ components/Main.js
@@ -0,0 +1,6 @@
+import r from "/render.js";
+
+export default () => {
+  console.log("hello");
+  return r.h1(null, "Prototype Starter");
+};
--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-import React from "./web_modules/react.js";
-import ReactDOM from "./web_modules/react-dom.js";
+import React from "/web_modules/react.js";
+import ReactDOM from "/web_modules/react-dom.js";
+import Main from "/components/Main.js";
 
-class Hello extends React.Component {
-  render() {
-    return React.createElement("h1", null, `Hello ${this.props.toWhat}`);
-  }
-}
-
-ReactDOM.render(
-  React.createElement(Hello, { toWhat: "World" }, null),
-  document.getElementById("main")
-);
+ReactDOM.render(React.createElement(Main), document.getElementById("main"));
